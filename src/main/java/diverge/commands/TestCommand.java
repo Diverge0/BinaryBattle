bbe3892f2f31b5fed0c094814de9c132ad086d98
Diverge0 <henning@die-ohlmeyers.de>
2020-10-16 01:00:10 +0200

    /test now stops/starts all player movement

--- src/main/java/diverge/commands/TestCommand.java
+++ src/main/java/diverge/commands/TestCommand.java
@@ -2,32 +2,46 @@ package main.java.diverge.commands;
 
 import main.java.diverge.BinaryBattle;
 
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.entity.living.player.Player;
-import org.spongepowered.api.text.Text;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.entity.MoveEntityEvent;
+import org.spongepowered.api.event.filter.cause.First;
 
 public class TestCommand implements CommandExecutor {
     //Instance of main class
     private final BinaryBattle plugin;
+    private boolean stopPlayer;
+    private Player player;
 
     public TestCommand(BinaryBattle plugin){
         this.plugin = plugin;
+        stopPlayer = false;
+        Sponge.getEventManager().registerListeners(plugin, this);
     }
 
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
         //Put Test code here
         if(src instanceof Player) {
-            Player player = (Player) src;
-            player.getInventory().clear();
+            this.player = (Player) src;
+            stopPlayer = !stopPlayer;
         }
 
 
 
         return CommandResult.success();
+    }
+
+    @Listener
+    public void onPlayerMove(MoveEntityEvent event, @First Player player){
+        if(stopPlayer){
+            event.setCancelled(true);
+        }
     }
 }

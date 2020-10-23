31b6d4ed9cad9e857a4e2c4821c87510b30a4b28
Diverge0 <henning@die-ohlmeyers.de>
2020-10-23 06:07:02 +0200

    battle init and battle start now working

--- src/main/java/diverge/commands/BattleCommand.java
+++ src/main/java/diverge/commands/BattleCommand.java
@@ -18,6 +18,7 @@ public class BattleCommand implements CommandExecutor {
     private String operationMode;
     public BattleCommand(BinaryBattle plugin, String operationMode){
         this.operationMode = operationMode;
+        this.plugin = plugin;
         Sponge.getEventManager().registerListeners(plugin, new Freeze());
     }
 
@@ -32,18 +33,31 @@ public class BattleCommand implements CommandExecutor {
             c.seconds = args.<Integer>getOne("time in seconds").get().intValue();
             c.channel = MessageChannel.TO_PLAYERS;
             Thread t = new Thread(c);//new Thread is needed. Server stops completely when waiting in main thread
+            Thread starter = new Start(t, plugin);
             t.start();
-            try{
-                t.join();
-            }
-            catch(InterruptedException e){
-                plugin.log("InterruptedException");
-            }
-            Freeze.stopPlayer = false;
-            MessageChannel.TO_ALL.send(Text.builder("Battle beginnt").color(TextColors.RED).build());
-
+            starter.start();
         }
 
         return CommandResult.success();
+    }
+    private class Start extends Thread{
+        private Thread t;
+
+        public Start(Thread t, BinaryBattle plugin){
+            this.t = t;
+        }
+
+        @Override
+        public void run() {
+            try {
+                t.join();
+            }
+            catch(InterruptedException e){
+                System.out.println("Uff");
+            }
+            Freeze.stopPlayer = false;
+            MessageChannel.TO_ALL.send(Text.builder("Battle beginnt").color(TextColors.DARK_RED).build());
+        }
+
     }
 }
--- src/main/java/diverge/commands/CountdownCommand.java
+++ src/main/java/diverge/commands/CountdownCommand.java
@@ -19,19 +19,4 @@ public class CountdownCommand implements CommandExecutor {
         new Thread(c).start();//new Thread is needed. Server stops completely when waiting in main thread
         return CommandResult.success();
     }
-
-    /*//Private class running in thread
-    private static class Countdown implements Runnable{
-        int seconds;
-        MessageChannel channel;
-
-        @Override
-        public void run(){
-            for (int i = seconds;i>=0; i--){
-                channel.send(Text.builder(Integer.toString(i)).color(TextColors.GOLD).build());
-                try{TimeUnit.SECONDS.sleep(1);}
-                catch(Exception e){}//useless catch block
-            }
-        }
-    }*/
 }
